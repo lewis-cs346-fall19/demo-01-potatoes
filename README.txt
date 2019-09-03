83d60dad23e5d69f97ee1b4222b1f5b1e9d63246
Martin Delaney Lamb <martinlamb@cs.arizona.edu>
2019-09-03 10:48:38 -0700

    net_client.py

---
+++ README.txt
@@ -0,0 +1,2 @@
+The client file will take the numbers from 1 to 100, send them to the server, and print the reply.
+The server file will receive the data, transform it once in the pattern of the Collatz conjecture, then send back the result.
---
+++ net_server.py
@@ -0,0 +1,24 @@
+import socket
+def main():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	addr = ("0.0.0.0", 17346)
+	sock.bind(addr)
+	sock.listen(5)
+	while True:
+		(connectedSock, clientAddress) = sock.accept()
+		while True:
+
+			try:
+				msg = connectedSock.recv(1024).decode()
+				if int(msg)%2==0:
+					returnMsg = int(msg)//2
+				elif int(msg)==1:
+					returnMsg = 1
+				else:
+					returnMsg = 3 *(int(msg)) + 1
+					print("///////////////////////////////////////////////")
+				connectedSock.sendall(str(returnMsg).encode())
+			except ConnectionAbortedError:
+				sock.close()
+				break
+main()
